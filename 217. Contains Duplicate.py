b0666f8724f679612fc11a0748ab634721b9c661
Rajesh650 <54935530+Rajesh650@users.noreply.github.com>
2024-03-01 14:02:44 +0530

    Add files via upload

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -49,13 +49,13 @@
     
 # ***************** HashSet (Time Complexity: O(n)) *********
 
-def ContainsDuplicate(nums):
-    seen = set()
-    for num in nums:
-        if num in seen:
-            return True
-            seen.add(num)
-    return False
+# def ContainsDuplicate(nums):
+#     seen = set()
+#     for num in nums:
+#         if num in seen:
+#             return True
+#             seen.add(num)
+#     return False
 
 #driver code
 # arr = [1,2,3,1]
